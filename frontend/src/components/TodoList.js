036bd29d9b491561f97987c5e2685e8803daeb6d
Nezumi <nezumi.shix@gmail.com>
2023-07-02 00:26:31 +0100

    First commit

---
+++ frontend/src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import Task from './Task';
+
+export default function TodoList() {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+
+    })
+}
